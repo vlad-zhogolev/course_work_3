8dd83f97e9bce8de4a55a0dc02d3503dc209ba3c
zvladyuha <z.vladyuha@yandex.ru>
2019-04-10 12:34:40 +0300

    fixed point fragment shader

--- CourseWork3/shaders/pbr.frag
+++ CourseWork3/shaders/pbr.frag
@@ -91,7 +91,7 @@ vec3 getNormalFromMap()
     vec2 st2 = dFdy(TexCoords);
 
     vec3 N   =  normalize(Normal);
-    vec3 T   =  normalize(Q1*st2.t - Q2*st1.t);
+    vec3 T   =  normalize(Q1 * st2.t - Q2 * st1.t);
     vec3 B   = -normalize(cross(N, T));
     mat3 TBN =  mat3(T, B, N);
 
--- CourseWork3/src/PointLight.h
+++ CourseWork3/src/PointLight.h
@@ -11,25 +11,24 @@ class PointLight : public Light
 public:
     PointLight(
         glm::vec3 position, glm::vec3 color,
-        glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, 
-        float constant, float linear, float quadratic);  
+        glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular,
+        float constant, float linear, float quadratic);
 
-    glm::vec3 getPosition() { return _position; }        
+    glm::vec3 getPosition() { return _position; }
     float getConstant() { return _constant; }
     float getLinear() { return _linear; }
     float getQuadratic() { return _quadratic; }
 
-    void setPosition(const glm::vec3& position) { _position = position; }  
+    void setPosition(const glm::vec3& position) { _position = position; }
     void setConstant(float constant) { _constant = (constant > 0) ? constant : 1.0; }
     void setLinear(float linear) { _linear = (linear >= 0) ? linear : 1.0; }
     void setQuadratic(float quadratic) { _quadratic = (quadratic >= 0)? quadratic : 1.0; }
 
  private:
-    glm::vec3 _position;    
+    glm::vec3 _position;
     float _constant;
     float _linear;
     float _quadratic;
 };
 
 #endif // !POINT_LIGHT_H
-
